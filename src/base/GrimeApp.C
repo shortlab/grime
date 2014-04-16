99f397861c1414ecd8e6d5d09d4d2342589d8c24
Michael Short <hereiam@mit.edu>
2014-04-16 10:48:58 -0400

    Folded in GRIME physics models

--- src/base/GrimeApp.C
+++ src/base/GrimeApp.C
@@ -1,7 +1,24 @@
 #include "GrimeApp.h"
 #include "Moose.h"
 #include "AppFactory.h"
-#include "ModulesApp.h"
+
+// Include kernel header files
+#include "DefectSink.h"
+#include "DefectSource.h"
+#include "DefectRecombination.h"
+#include "MatPropDiffusion.h"
+
+// Include auxkernel header files
+#include "DislocationSinkRate.h"
+#include "DefectRecombinationRateConstant.h"
+#include "ExcessInterstitialFractionAux.h"
+#include "VacancySupersaturation.h"
+#include "VacancySupersaturationNumerator.h"
+#include "VoidNucleation.h"
+#include "VoidSinkRate.h"
+
+// Next come the materials
+#include "RadiationMaterial.h"
 
 template<>
 InputParameters validParams<GrimeApp>()
@@ -14,13 +31,11 @@ GrimeApp::GrimeApp(const std::string & name, InputParameters parameters) :
     MooseApp(name, parameters)
 {
   srand(libMesh::processor_id());
-  
+
   Moose::registerObjects(_factory);
-  ModulesApp::registerObjects(_factory);
   GrimeApp::registerObjects(_factory);
 
   Moose::associateSyntax(_syntax, _action_factory);
-  ModulesApp::associateSyntax(_syntax, _action_factory);
   GrimeApp::associateSyntax(_syntax, _action_factory);
 }
 
@@ -37,6 +52,23 @@ GrimeApp::registerApps()
 void
 GrimeApp::registerObjects(Factory & factory)
 {
+  // Register kernels
+  registerKernel(DefectSink);
+  registerKernel(DefectSource);
+  registerKernel(DefectRecombination);
+  registerKernel(MatPropDiffusion);
+
+  // Register auxkernels
+  registerAux(DislocationSinkRate);
+  registerAux(DefectRecombinationRateConstant);
+  registerAux(ExcessInterstitialFractionAux);
+  registerAux(VacancySupersaturation);
+  registerAux(VacancySupersaturationNumerator);
+  registerAux(VoidNucleation);
+  registerAux(VoidSinkRate);
+
+  // Register materials classes
+  registerMaterial(RadiationMaterial);
 }
 
 void
